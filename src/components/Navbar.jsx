2d2d01ae3df0be18e6b62d0a050e4fa05834e429
Tyler Woyshner <tylerwoyshner@Tylers-MacBook-Pro.local>
2024-05-23 17:27:03 -0400

    readme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import './Navbar.css';
 
 const Navbar = () => {
   return (
     <>
-        <Link to='/'>Home</Link>
-        <Link to='/players'>All Players</Link>
+      <div className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/players">All Players</Link>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Navbar;
